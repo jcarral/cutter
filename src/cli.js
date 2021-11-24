598372a5329298ea631ddb2485ea8ac5d29a69fe
Joseba Carral <jcarraldc@gmail.com>
2021-11-24 17:35:42 +0100

    First working function

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,22 @@
 const arg = require('arg');
 
-const main = async args => {
-    console.log('Initial setup')
-}
+const FLAGS = {
+    '--input': String,
+    '--output': String,
+    '--size': Number,
+    '--files': Number,
 
-module.exports.cli = main;
+    '-i': '--input',
+    '-o': '--output',
+    '-s': '--size',
+    '-f': '--files',
+};
+
+module.exports.parse = () => {
+    const { _, ...args } = arg(
+        FLAGS
+    );
+    let parsedOptions = {};
+    Object.keys(args).forEach(key => parsedOptions[key.replace('--', '')] = args[key]);
+    return parsedOptions;
+};
